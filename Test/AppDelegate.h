c8fbcd49e428bf3393b62eef8fb71fc801606d64
김승준 <sjkim@midasit.com>
2016-06-15 07:55:11 +0900

    add to github

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Seungjun Kim on 2016. 6. 14..
+//  Copyright © 2016년 Seungjun Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Seungjun Kim on 2016. 6. 14..
+//  Copyright © 2016년 Seungjun Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
